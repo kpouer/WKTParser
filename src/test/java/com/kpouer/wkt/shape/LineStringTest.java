8f87df0e9f68db217ea137c318fae72d146528e0
Matthieu Casanova <public@kpouer.com>
2023-02-23 14:51:25 +0100

    parametize test

--- src/test/java/com/kpouer/wkt/shape/LineStringTest.java
+++ src/test/java/com/kpouer/wkt/shape/LineStringTest.java
@@ -3,6 +3,8 @@ package com.kpouer.wkt.shape;
 import com.kpouer.wkt.ParseException;
 import com.kpouer.wkt.WKT;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,35 +17,18 @@ class LineStringTest {
         assertEquals(0, lineString.getBarycenter().getY());
     }
 
-    @Test
-    void testBarycenter2Points() throws ParseException {
-        LineString lineString = WKT.parseShape("LINESTRING(5 5,10 10)");
-        assertEquals(2, lineString.getNpoints());
-        assertEquals(7.5, lineString.getBarycenter().getX());
-        assertEquals(7.5, lineString.getBarycenter().getY());
-    }
-
-    @Test
-    void testBarycenter2PointsReversed() throws ParseException {
-        LineString lineString = WKT.parseShape("LINESTRING(10 10, 5 5)");
-        assertEquals(2, lineString.getNpoints());
-        assertEquals(7.5, lineString.getBarycenter().getX());
-        assertEquals(7.5, lineString.getBarycenter().getY());
-    }
-
-    @Test
-    void testBarycenterNPoints() throws ParseException {
-        LineString lineString = WKT.parseShape("LINESTRING(5 5,5 10, 10 10, 10 5)");
-        assertEquals(4, lineString.getNpoints());
-        assertEquals(7.5, lineString.getBarycenter().getX());
-        assertEquals(7.5, lineString.getBarycenter().getY());
-    }
-
-    @Test
-    void testBarycenterNPointsReversed() throws ParseException {
-        LineString lineString = WKT.parseShape("LINESTRING(10 10, 10 5,5 5,5 10)");
-        assertEquals(4, lineString.getNpoints());
-        assertEquals(7.5, lineString.getBarycenter().getX());
-        assertEquals(7.5, lineString.getBarycenter().getY());
+    @ParameterizedTest
+    @CsvSource(value = {
+        "LINESTRING(5 5,10 10); 2; 7.5; 7.5",
+        "LINESTRING(10 10, 5 5); 2; 7.5; 7.5",
+        "LINESTRING(5 5,5 10, 10 10, 10 5); 4; 7.5; 7.5",
+        "LINESTRING(10 10, 10 5,5 5,5 10); 4; 7.5; 7.5"
+    },
+    delimiter = ';')
+    void testBarycenter2PointsParametized(String wkt, int nPoints, double x, double y) throws ParseException {
+        LineString lineString = WKT.parseShape(wkt);
+        assertEquals(nPoints, lineString.getNpoints());
+        assertEquals(x, lineString.getBarycenter().getX());
+        assertEquals(y, lineString.getBarycenter().getY());
     }
 }
