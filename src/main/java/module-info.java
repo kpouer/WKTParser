b9e8e582fd3963029cb9b2c778daa346074b8a88
Matthieu Casanova <chocolat.mou@gmail.com>
2023-03-25 23:05:09 +0100

    jakarta annotations should not be required in runtime

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,6 @@
 module com.kpouer.wkt {
     requires static lombok;
-    requires jakarta.annotation;
+    requires static jakarta.annotation;
     requires java.desktop;
     exports com.kpouer.wkt;
     exports com.kpouer.wkt.shape;
